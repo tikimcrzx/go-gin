a0a9590469469c9a41b04f52ce40782ff2c1c1ba
tikimcrzx <tikimioo723@gmail.com>
2021-09-21 20:07:58 -0700

    recipe data model

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+	"time"
 
-func IndexHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Cossette",
-	})
+	"github.com/gin-gonic/gin"
+)
+
+var recipes []Recipe
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
 
 func main() {
@@ -13,5 +22,18 @@ func main() {
 
 	router.GET("/", IndexHandler)
 
-	router.Run()
+	router.Run(":5000")
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(200, Person{
+		FirstName: "Cossette",
+		LastName:  "Potrait"},
+	)
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
 }
